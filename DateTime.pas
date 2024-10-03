87c66829441245428f9ce4723769e33319d84ab9
Andrey Khrobostov <ansergoff@gmail.com>
2024-10-03 09:22:24 +0300

    Fixed bugs

--- DateTime.pas
+++ DateTime.pas
@@ -2,9 +2,9 @@
   var nowYear := dateTime.Now.Year;
   if nowYear mod 100 = 0 then
     if nowYear mod 400 = 0 then
-      print('год високонсный');
-    else print('год не високонсный');
+      print('год високонсный')
+    else print('год не високонсный')
   else if nowYear mod 4 = 0 then
-    print('год високонсный');
-  else ('год не високонсный');
+    print('год високонсный')
+  else print('год не високонсный');
 end.
