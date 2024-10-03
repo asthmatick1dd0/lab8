0e7f810ade8cbc029c1375e90152f0f4f3749b97
Andrey Khrobostov <ansergoff@gmail.com>
2024-10-03 09:20:21 +0300

    Add is leap year program

---
+++ DateTime.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var nowYear := dateTime.Now.Year;
+  if nowYear mod 100 = 0 then
+    if nowYear mod 400 = 0 then
+      print('год високонсный');
+    else print('год не високонсный');
+  else if nowYear mod 4 = 0 then
+    print('год високонсный');
+  else ('год не високонсный');
+end.
