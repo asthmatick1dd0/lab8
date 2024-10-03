946e7b6175f3e2a3d44788c2b89092d1ebc31653
Andrey Khrobostov <ansergoff@gmail.com>
2024-10-03 09:56:49 +0300

    Add new feature

--- hello.pas
+++ hello.pas
@@ -1,2 +1,8 @@
 ﻿##
-print('Hello world!');
+var hour := ReadInteger('Введите ваш час');
+case hour of
+  4..10: print('Доброе утро, мир!');
+  11..16: print('Добрый день, мир!');
+  17..22: print('Добрый вечер, мир!');
+  else print('Доброй ночи, мир!');
+end;
