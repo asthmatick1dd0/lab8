40c3dfbc24f3308d85db091567536c10ff7c28b8
Andrey Khrobostov <ansergoff@gmail.com>
2024-10-03 10:03:45 +0300

    Add a new program

---
+++ timer.pas
@@ -0,0 +1,6 @@
+﻿##
+var hours := ReadInteger('Введтие количество часов');
+var mins := ReadInteger('Введтие количество минут');
+var seconds := ReadInteger('Введтие количество секунд');
+
+print($'Суммарное количество секунд = {hours*60*60+mins*60+seconds}');
